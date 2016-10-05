84009ea5db293c2aab0e3544cb33c41ca93cd8b9
rhinodavid <dawalsh@gmail.com>
2016-10-05 15:36:45 -0700

    Send contents of website in archive

--- web/request-handler.js
+++ web/request-handler.js
@@ -5,12 +5,13 @@ var url = require('url');
 var fs = require('fs');
 
 exports.handleRequest = function (req, res) {
+  var urlPath = url.parse(req.url);
+  var pathName = urlPath.pathname;
+  var filePath;
   //if req is to root or index
-  if (req.method === 'GET') {
+  if (req.method === 'GET' && (pathName === '/' || pathName === '/index.html')) {
     //get file path from the url
-    var urlPath = url.parse(req.url);
-    var pathName = urlPath.pathname;
-    var filePath = pathName === '/' ? path.join(archive.paths.publicSites, 'index.html') : path.join(archive.paths.publicSites, pathName);
+    filePath = pathName === '/' ? path.join(archive.paths.publicSites, 'index.html') : path.join(archive.paths.publicSites, pathName);
     fs.readFile(filePath, function(err, data) {
       if (err) {
         res.statusCode = 404;
@@ -20,7 +21,20 @@ exports.handleRequest = function (req, res) {
         res.end(data);
       }
     });
-
+  } else if (req.method === 'GET') {
+    // get the website URL the user is interested in ===> pathName
+    // convert to filepath
+    filePath = path.join(archive.paths.archivedSites, pathName);
+    // read file
+    fs.readFile(filePath, function(err, data) {
+      if (err) {
+        res.statusCode = 404;
+        res.end('File not found.');
+      } else {
+        res.statusCode = 200;
+        res.end(data);
+      }
+    });
   }
   // res.end(archive.paths.list);
 };
