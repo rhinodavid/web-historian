d13e0aeb2e6a948cf5dfbe43a581049c4ab20481
rhinodavid <dawalsh@gmail.com>
2016-10-05 15:18:14 -0700

    Implement static file serving

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -12,7 +12,8 @@ var _ = require('underscore');
 exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
   archivedSites: path.join(__dirname, '../archives/sites'),
-  list: path.join(__dirname, '../archives/sites.txt')
+  list: path.join(__dirname, '../archives/sites.txt'),
+  publicSites: path.join(__dirname, '../web/public')
 };
 
 // Used for stubbing paths for tests, do not modify
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,26 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var url = require('url');
+var fs = require('fs');
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  //if req is to root or index
+  if (req.method === 'GET') {
+    //get file path from the url
+    var urlPath = url.parse(req.url);
+    var pathName = urlPath.pathname;
+    var filePath = pathName === '/' ? path.join(archive.paths.publicSites, 'index.html') : path.join(archive.paths.publicSites, pathName);
+    fs.readFile(filePath, function(err, data) {
+      if (err) {
+        res.statusCode = 404;
+        res.end('File not found.');
+      } else {
+        res.statusCode = 200;
+        res.end(data);
+      }
+    });
+
+  }
+  // res.end(archive.paths.list);
 };
