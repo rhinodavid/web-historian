e435aaaa0cab0bb13417c5703979e7a57a58d3ac
rhinodavid <dawalsh@gmail.com>
2016-10-05 20:41:40 -0700

    Implement fetcher worker

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,15 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+
+var archive = require('../helpers/archive-helpers');
+
+archive.readListOfUrls(function(urls) {
+  if (urls) {
+    urls.forEach(function(url) {
+      if (url !== '') {
+        archive.downloadUrl(url);
+      }
+    });
+    archive.clearSitesList();
+  }
+});
